3a7e481d6aa1db5ba415a66fd9d327bd9fed2276
Ray Winkelman <raywinkelman@gmail.com>
2018-03-11 22:09:17 -0300

    Deploy script patch

--- utils/deploy_prod.sh
+++ utils/deploy_prod.sh
@@ -3,6 +3,6 @@
 cd ../API/
 /opt/plesk/php/7.1/bin/php /composer.phar install
 /opt/plesk/php/7.1/bin/php artisan cache:clear
-/opt/plesk/php/7.1/bin/php composer.phar dump-autoload -o
+/opt/plesk/php/7.1/bin/php /composer.phar dump-autoload -o
 /opt/plesk/php/7.1/bin/php /composer.phar update
 /opt/plesk/php/7.1/bin/php artisan migrate
